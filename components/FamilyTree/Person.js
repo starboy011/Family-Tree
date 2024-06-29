adf64a7932f530ac91c2c9cf1328104ea7872d9f
starboy011 <rishavofficial294@gmail.com>
2024-06-29 21:03:42 +0100

    till date

---
+++ components/FamilyTree/Person.js
@@ -0,0 +1,51 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+const Person = ({ name }) => {
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          width: 125,
+          height: 100,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{
+            width: 95,
+            height: 95,
+            borderRadius: 50,
+            backgroundColor: "red",
+          }}
+        ></View>
+      </View>
+
+      <View
+        style={{
+          flex: 1,
+          height: 100,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ fontFamily: "serif", fontSize: 20 }}>{name}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Person;
+
+const styles = StyleSheet.create({
+  container: {
+    width: "98%",
+    height: 100,
+    marginTop: 10,
+    display: "flex",
+    flexDirection: "row",
+    borderRadius: 20,
+  },
+});
