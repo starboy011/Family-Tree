1bc5b34f5f139d5cedc14f6d8f6501372b76c3e5
starboy011 <rishavofficial294@gmail.com>
2024-06-29 08:21:02 +0100

    starting family tree

--- components/FamilyTree/FamilyTree.js
+++ components/FamilyTree/FamilyTree.js
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-
+import { SafeAreaView } from "react-native-safe-area-context";
 const FamilyTree = () => {
   return (
-    <View>
-      <Text>FamilyTree</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>FamilyTree</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
--- components/Search/Search.js
+++ components/Search/Search.js
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-
+import { SafeAreaView } from "react-native-safe-area-context";
 const Search = () => {
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>Search</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
