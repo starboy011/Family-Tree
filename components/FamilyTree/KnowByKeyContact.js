c742435a9cbd1f2beb718ba0b39214e4047c08fe
starboy011 <rishavofficial294@gmail.com>
2024-07-14 19:39:50 +0100

    cahnged padding

--- components/FamilyTree/KnowByKeyContact.js
+++ components/FamilyTree/KnowByKeyContact.js
@@ -80,7 +80,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    padding: 20,
+    paddingTop: 10,
   },
   loadingContainer: {
     flex: 1,
