e39485b4575d2f35793b18e27284f6af303e26f8
starboy011 <rishavofficial294@gmail.com>
2024-07-14 13:23:39 +0100

    changed search bar logic

--- components/FamilyTree/FamilyTree.js
+++ components/FamilyTree/FamilyTree.js
@@ -1,73 +1,49 @@
-import React, { useState, useEffect } from "react";
-import {
-  StyleSheet,
-  View,
-  ScrollView,
-  ActivityIndicator,
-  Modal,
-  Text,
-  TouchableOpacity,
-} from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import Person from "./Person";
-import { Searchbar } from "react-native-paper";
+import { Searchbar, Button } from "react-native-paper";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 
 const FamilyTree = () => {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [persons, setPersons] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [showModal, setShowModal] = useState(false); // State for modal visibility
-  const [selectedPerson, setSelectedPerson] = useState(null); // State to store the selected person
   const navigation = useNavigation();
-
-  useEffect(() => {
-    fetch("http://192.168.68.123:8080/names")
-      .then((response) => response.json())
-      .then((data) => {
-        const updatedPersons = data.map((item) => ({
-          id: item.id,
-          name: item.data.name,
-          generation: item.generation,
-        }));
-        setPersons(updatedPersons);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        setLoading(false);
-      });
-  }, []);
-
-  const filteredPersons = persons.filter((person) =>
-    person.name.toLowerCase().includes(searchQuery.toLowerCase())
-  );
-
-  const handlePersonPress = (person) => {
-    if (person.generation < 5) {
-      setSelectedPerson(person);
-      setShowModal(true);
-    } else {
-      navigation.navigate("FamilyTreeWithId", {
-        personId: person.id,
-      });
-    }
-  };
-
-  const closeModal = () => {
-    setShowModal(false);
-  };
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.searchContainer}>
-        <Searchbar
-          placeholder="Search by name"
-          onChangeText={setSearchQuery}
-          value={searchQuery}
-          style={{ width: "80%" }}
-        />
+        <View
+          style={{
+            width: "80%",
+            backgroundColor: "rgba(79,255,176,.1)",
+            height: "60%",
+            borderRadius: 10,
+            flexDirection: "row",
+          }}
+        >
+          <View
+            style={{
+              width: "70%",
+              height: "100%",
+              justifyContent: "center",
+            }}
+          >
+            <Text
+              style={{
+                marginLeft: 10,
+                fontSize: 25,
+                color: "black",
+                textShadowColor: "rgba(0, 0, 0, .5)",
+                textShadowOffset: { width: -1, height: 1 },
+                textShadowRadius: 10,
+                textTransform: "uppercase",
+                letterSpacing: 2,
+              }}
+            >
+              VIEW FULL TREE
+            </Text>
+          </View>
+          <View style={{ width: "30%", height: "100%" }}></View>
+        </View>
         <View
           style={{
             width: 50,
@@ -83,40 +59,6 @@ const FamilyTree = () => {
           </TouchableOpacity>
         </View>
       </View>
-      {loading ? (
-        <View style={styles.loaderContainer}>
-          <ActivityIndicator size="large" color="green" />
-        </View>
-      ) : (
-        <ScrollView contentContainerStyle={styles.scrollViewContent}>
-          {filteredPersons.map((person, index) => (
-            <View key={index} style={styles.personContainer}>
-              <TouchableOpacity onPress={() => handlePersonPress(person)}>
-                <Person name={person.name} />
-              </TouchableOpacity>
-            </View>
-          ))}
-        </ScrollView>
-      )}
-
-      {/* Modal */}
-      <Modal
-        visible={showModal}
-        animationType="slide"
-        transparent={true}
-        onRequestClose={closeModal} // For Android back button
-      >
-        <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
-            <Text style={styles.modalText}>
-              Tree size is huge. Please narrow down your search.
-            </Text>
-            <TouchableOpacity style={styles.modalButton} onPress={closeModal}>
-              <Text style={styles.modalButtonText}>Close</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </Modal>
     </SafeAreaView>
   );
 };
