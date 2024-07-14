e0c711f65ef4130dac55bd3da47194274bcdd746
starboy011 <rishavofficial294@gmail.com>
2024-07-14 17:51:22 +0100

    till date

---
+++ components/FamilyTree/KnowByNameContainer.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const KnowByNameContainer = () => {
+  return (
+    <View>
+      <Text>KnowByNameContainer</Text>
+    </View>
+  );
+};
+
+export default KnowByNameContainer;
+
+const styles = StyleSheet.create({});
