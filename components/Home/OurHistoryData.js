cfe9d33673ab7bfdd869b418d697ad63ac81bf6f
starboy011 <rishavofficial294@gmail.com>
2024-07-23 20:31:36 +0100

    added details

--- components/Home/OurHistoryData.js
+++ components/Home/OurHistoryData.js
@@ -6,10 +6,14 @@ import {
   ImageBackground,
 } from "react-native";
 import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Appbar } from "react-native-paper";
 import Carousel from "react-native-snap-carousel";
+import { useNavigation } from "@react-navigation/native";
+import { ScrollView } from "react-native-gesture-handler";
+import { LinearGradient } from "expo-linear-gradient";
 const { width } = Dimensions.get("window");
 const OurHistoryData = () => {
+  const navigation = useNavigation();
   const data = [
     {
       title: "Slide 1",
@@ -129,29 +133,153 @@ const OurHistoryData = () => {
     }
   };
   return (
-    <SafeAreaView>
-      <View
-        style={{
-          width: "100%",
-          height: "100%",
-          backgroundColor: "white",
-        }}
-      >
-        <View style={styles.carouselContainer}>
-          <Carousel
-            data={data}
-            renderItem={renderItem}
-            sliderWidth={width * 1}
-            itemWidth={width * 0.85}
-            inactiveSlideScale={0.9}
-            inactiveSlideOpacity={0.6}
-            activeSlideAlignment={"center"}
-            containerCustomStyle={styles.carousel}
-            contentContainerCustomStyle={styles.carouselContentContainer}
-          />
+    <>
+      <Appbar.Header style={{ backgroundColor: "#F0F8FF" }}>
+        <Appbar.BackAction onPress={() => navigation.goBack()} />
+        <Appbar.Content title="Home" />
+      </Appbar.Header>
+      <ScrollView>
+        <View
+          style={{
+            width: "100%",
+            height: "100%",
+            backgroundColor: "white",
+          }}
+        >
+          <View style={styles.carouselContainer}>
+            <Carousel
+              data={data}
+              renderItem={renderItem}
+              sliderWidth={width * 1}
+              itemWidth={width * 0.85}
+              inactiveSlideScale={0.9}
+              inactiveSlideOpacity={0.6}
+              activeSlideAlignment={"center"}
+              containerCustomStyle={styles.carousel}
+              contentContainerCustomStyle={styles.carouselContentContainer}
+              autoplay={true}
+              autoplayInterval={3000}
+              autoplayDelay={1000}
+            />
+          </View>
+
+          <View style={styles.detail1}>
+            <View style={{ backgroundColor: "#F0F8FF" }}>
+              <View
+                style={{
+                  alignItems: "center",
+                }}
+              >
+                <LinearGradient
+                  colors={["#4c669f", "#3b5998", "#192f6a"]}
+                  style={{
+                    flex: 1,
+                    marginTop: 10,
+                    height: 60,
+                    justifyContent: "center",
+                    borderRadius: 10,
+                    width: "95%",
+                  }}
+                  start={{ x: 0, y: 0 }}
+                  end={{ x: 1, y: 0 }}
+                >
+                  <Text
+                    style={{
+                      fontSize: 40,
+                      fontWeight: "bold",
+                      color: "white",
+                      textAlign: "center",
+                    }}
+                  >
+                    कान्यकुब्ज की उत्पत्ति
+                  </Text>
+                </LinearGradient>
+              </View>
+              <View
+                style={{
+                  width: "98%",
+                  paddingTop: 20,
+                  paddingLeft: 20,
+                }}
+              >
+                <Text style={{ fontSize: 20 }}>
+                  कहा जाता है कि आदिकाल में महोदयपुर राज्य के राजा ‘कुशनाभ’ की
+                  एक रूपवती राजकुमारी थी । जिसपर मोहित होकर वायुदेव ने उससे
+                  विवाह का प्रस्ताव रखा, परंतु पिता की आज्ञा ना होने के कारण
+                  कन्या ने वायुदेव का प्रस्ताव ठुकरा दिया। जिसके परिणाम स्वरुप
+                  वायुदेव कुपित हो गए और राजकुमारी को ‘कुब्जा’ होने का श्राप दे
+                  डाला। इस घटना के ज्ञात होने के उपरांत कन्या के पिता ने अपनी
+                  पुत्री का विवाह महर्षि ब्रम्हदत्त से कर दिया और आशीर्वाद
+                  स्वरुप पाणिग्रहण होते ही कन्या पुन: रूपवती हो गई। जिस क्षेत्र
+                  में वह कन्याकुब्जा रहा करती थी उस क्षेत्र को कान्यकुब्ज
+                  क्षेत्र के नाम से जाना जाने लगा जो अयोध्या के दक्षिण में
+                  श्रृंगवेरपुर से दालभ्य ऋषि के आश्रम तक फैला हुआ था। वर्तमान
+                  में यह कन्नौज तथा इससे सटा अवध का क्षेत्र है। इस क्षेत्र में
+                  बसने वाले ब्राम्हण कान्यकुब्ज ब्राह्मण कहलाए।
+                </Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.detail2}>
+            <View style={{ backgroundColor: "rgba(200, 247, 197,.4)" }}>
+              <View
+                style={{
+                  alignItems: "center",
+                }}
+              >
+                <LinearGradient
+                  colors={[
+                    "rgba(200, 247, 197,.4)",
+                    "rgba(101, 198, 187,1)",
+                    "rgba(27, 163, 156,1)",
+                  ]}
+                  style={{
+                    flex: 1,
+                    marginTop: 10,
+                    height: 60,
+                    justifyContent: "center",
+                    borderRadius: 10,
+                    width: "95%",
+                  }}
+                  start={{ x: 0, y: 0 }}
+                  end={{ x: 1, y: 0 }}
+                >
+                  <Text
+                    style={{
+                      fontSize: 40,
+                      fontWeight: "bold",
+                      color: "white",
+                      textAlign: "center",
+                    }}
+                  >
+                    सांकृत ग़ोत्र
+                  </Text>
+                </LinearGradient>
+              </View>
+              <View
+                style={{
+                  width: "98%",
+                  paddingTop: 20,
+                  paddingLeft: 20,
+                }}
+              >
+                <Text style={{ fontSize: 20 }}>
+                  ब्रह्माजी के पुत्र भृगुके वंश में संख्यायन ऋषि हुए तथा उनके
+                  पुत्र गगन और गगन के पुत्र ऋषि सांकृत हुए ।अतएव इन्हीं ऋषि के
+                  नाम से कान्यकुब्ज का गोत्र जाना जाता है । सांकृत गोत्र का वेद
+                  “यजुर्वेद” और उपवेद “धनुर्वेद” है।सांकृत गोत्रधारी शिखा को दाई
+                  और घुमाकर बांधते हैं और इस गोत्र के अस्पद हैं “चौबे”।अस्पद
+                  अर्थात वो प्रजातियां जो किसी व्यक्ति या स्थान के नाम से जानी
+                  गई।कहा जाता है ये चौबे से पूर्व चतुर्वेदी हुआ करते थे।सांकृत
+                  गोत्र के इष्टदेव शिव माने जाते हैं। इस विचारधारा में समय-समय
+                  पर परिवर्तन होता रहा है।
+                </Text>
+              </View>
+            </View>
+          </View>
         </View>
-      </View>
-    </SafeAreaView>
+      </ScrollView>
+    </>
   );
 };
 
@@ -181,5 +309,11 @@ const styles = StyleSheet.create({
   slideText: {
     fontSize: 16,
     fontWeight: "bold",
+  },
+  detail1: {
+    width: "100%",
+  },
+  detail2: {
+    width: "100%",
   },
 });
