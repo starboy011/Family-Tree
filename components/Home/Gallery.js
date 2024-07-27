814ede41d20c33498a28b4e31b0ca8cbb02a7ea2
starboy011 <rishavofficial294@gmail.com>
2024-07-27 16:06:28 +0100

    image gallery completed

--- components/Home/Gallery.js
+++ components/Home/Gallery.js
@@ -1,12 +1,50 @@
-import { StyleSheet, Text, View, ImageBackground } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  ScrollView,
+} from "react-native";
+import React, { useState, useEffect } from "react";
 import { Appbar } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
-import { ScrollView } from "react-native-gesture-handler";
+import axios from "axios";
 
 const Gallery = () => {
   const navigation = useNavigation();
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    axios
+      .get("http://192.168.68.123:8080/imageGallery")
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+  }, []);
+  if (loading) {
+    return (
+      <View
+        style={{
+          height: "100%",
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "white",
+        }}
+      >
+        <ImageBackground
+          source={require("./SplashScreen.gif")}
+          style={{ width: 100, height: 100 }}
+        ></ImageBackground>
+      </View>
+    );
+  }
   return (
     <>
       <Appbar.Header style={{ backgroundColor: "#F0F8FF" }}>
@@ -14,46 +52,56 @@ const Gallery = () => {
         <Appbar.Content title="Home" />
       </Appbar.Header>
       <ScrollView>
-        <View
-          style={{
-            width: "100%",
-            backgroundColor: "white",
-            height: 300,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <ImageBackground
-            source={require("./ContentBy.png")}
-            style={{
-              width: "95%",
-              height: "95%",
-              borderRadius: 10,
-              overflow: "hidden",
-              justifyContent: "flex-end",
-              //   borderWidth: 1,
-            }}
-            imageStyle={{
-              width: "100%",
-              height: "100%",
-              resizeMode: "center",
-            }}
-          >
-            <View style={{ backgroundColor: "rgba(0,0,0,0.5)" }}>
-              <Text
+        {loading ? (
+          <Text>Loading...</Text>
+        ) : (
+          data.map((item, index) => (
+            <View
+              key={index}
+              style={{
+                width: "100%",
+                backgroundColor: "white",
+                height: 300,
+                justifyContent: "center",
+                alignItems: "center",
+                // marginBottom: 10,
+              }}
+            >
+              <ImageBackground
+                source={{ uri: item.Img }}
                 style={{
-                  color: "white",
-                  textAlign: "center",
-                  textAlignVertical: "center",
-                  fontFamily: "serif",
-                  fontSize: 25,
+                  width: "95%",
+                  height: "95%",
+                  borderRadius: 10,
+                  overflow: "hidden",
+                  justifyContent: "flex-end",
                 }}
+                imageStyle={{
+                  width: "100%",
+                  height: "100%",
+                  resizeMode: "stretch",
+                }}
+                onError={(error) =>
+                  console.error("Image loading error:", error)
+                }
               >
-                Rishav
-              </Text>
+                <View style={{ backgroundColor: "rgba(0,0,0,0.5)" }}>
+                  <Text
+                    style={{
+                      color: "white",
+                      textAlign: "center",
+                      textAlignVertical: "center",
+                      fontFamily: "serif",
+                      fontSize: 25,
+                    }}
+                  >
+                    {item.Name}
+                  </Text>
+                </View>
+              </ImageBackground>
             </View>
-          </ImageBackground>
-        </View>
+          ))
+        )}
       </ScrollView>
     </>
   );
